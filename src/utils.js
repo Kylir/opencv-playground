a6a91dfed6355be6b40939e64acae147e9be72f1
Kylir <jason.forest@glyphe.org>
2018-12-12 01:01:52 +0000

    fix export issue

--- src/utils.js
+++ src/utils.js
@@ -6,7 +6,7 @@ const cv = require('opencv4nodejs')
 //icol = (89, 0, 0, 125, 255, 255)  // Blue
 //icol = (0, 100, 80, 10, 255, 255)   // Red
 
-export function processImage (image) {
+function processImage (image) {
     return image.resize(300, 300).gaussianBlur(new cv.Size(5,5), 0)
 }
 
@@ -17,7 +17,7 @@ export function processImage (image) {
  * @param {boolean} debug optional - wil open the filtered image if set to true
  * @returns {Array} the contours of the red objects
  */
-export function findRedContours (image, debug) {
+function findRedContours (image, debug) {
     // Red is at the upper and lower Hue - we need two masks
     const lowRed_mask_up = new cv.Vec3(170, 100, 100)
     const highRed_mask_up = new cv.Vec3(180, 255, 255)
@@ -42,7 +42,7 @@ export function findRedContours (image, debug) {
  * @param {boolean} debug Optional parameter. if set to true it will try to open the result of the filter
  * @returns {Array} the contours of the Green objects
  */
-export function findGreenContours (image, debug) {
+function findGreenContours (image, debug) {
     const highGreen_mask = new cv.Vec3(71, 255, 255)
     const lowGreen_mask = new cv.Vec3(36, 202, 59)
     const green = image.cvtColor(cv.COLOR_BGR2HSV).inRange(lowGreen_mask, highGreen_mask)
@@ -62,7 +62,7 @@ export function findGreenContours (image, debug) {
  * @param {boolean} debug Optional parameter. if set to true it will try to open the result of the filter
  * @returns {Array} the contours of the blue objects
  */
-export function findBlueContours (image, debug) {
+function findBlueContours (image, debug) {
     const highBlue_mask = new cv.Vec3(125, 255, 255)
     const lowBlue_mask = new cv.Vec3(89, 0, 0)
     const blue = image.cvtColor(cv.COLOR_BGR2HSV).inRange(lowBlue_mask, highBlue_mask)
@@ -82,7 +82,7 @@ export function findBlueContours (image, debug) {
  * @param {boolean} debug Optional parameter. if set to true it will try to open the result of the filter
  * @returns {Array} the contours of the Yellow objects
  */
-export function findYellowContours (image, debug) {
+function findYellowContours (image, debug) {
     const highYellow_mask = new cv.Vec3(36, 255, 255)
     const lowYellow_mask = new cv.Vec3(18, 0, 196)
     const yellow = image.cvtColor(cv.COLOR_BGR2HSV).inRange(lowYellow_mask, highYellow_mask)
@@ -102,7 +102,7 @@ export function findYellowContours (image, debug) {
  * @param {Array of Contour} contours
  * @returns {Contour} the contour with the biggest area 
  */
-export function findBiggestArea (contours) {
+function findBiggestArea (contours) {
     return contours.reduce((biggest, contour) => {
         if (contour.area > biggest.area) {
             return contour
@@ -117,7 +117,7 @@ export function findBiggestArea (contours) {
  * @param {Contour} contour
  * @returns {cX, cY} the coordinates of the center 
  */
-export function findCentre (contour) {
+function findCentre (contour) {
     const m = contour.moments()
 	const cX = Math.round(m.m10 / m.m00)
     const cY = Math.round(m.m01 / m.m00)
@@ -129,9 +129,9 @@ export function findCentre (contour) {
  * @param {number} xShape
  * @param {number} imageWidth
  */
-export function xAxisDeviation (xShape, imageWidth) {
+function xAxisDeviation (xShape, imageWidth) {
     return (imageWidth/2 - xShape)
 }
 
-export default {processImage, findRedContours, findGreenContours, findBlueContours, findYellowContours, findBiggestArea, findCentre, xAxisDeviation}
+module.exports = {processImage, findRedContours, findGreenContours, findBlueContours, findYellowContours, findBiggestArea, findCentre, xAxisDeviation}
 
