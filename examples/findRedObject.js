55f5a152d2fe2e9c92a35112a170ac3311e32a51
Kylir <jason.forest@glyphe.org>
2019-01-10 17:15:54 +0000

    Fix typos

--- examples/findRedObject.js
+++ examples/findRedObject.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 const cvUtils = require('../src/opencv-utils')
-const robotUtils = require('../src/robot-utils')
+//const robotUtils = require('../src/robot-utils')
 
 // DEMO FOR VIDEO CAPTURE
 function demoRobotMove () {
@@ -13,7 +13,7 @@ function demoRobotMove () {
 
         if (cont && (cont.length > 0)) {
             const big = cvUtils.findBiggestArea(cont)
-            console.log(`${cont.length} red objects. Biggest at (${c.cX}, ${c.cY}) with area ${big.area}.`)
+            console.log(`${cont.length} red objects. Biggest at (${cont.cX}, ${cont.cY}) with area ${big.area}.`)
             if (big.area > 0.1) {
                 const c = cvUtils.findCentre(big)
                 const deviation = cvUtils.xAxisDeviation(c.cX, 300)
@@ -25,13 +25,8 @@ function demoRobotMove () {
             }
             
         } else {
-<<<<<<< HEAD
-            console.log('No red objects found...')
-            robotUtils.circle(100, 100)
-=======
-            console.log(`No red objects found...`)
+            console.log('No red objects found. Circling...')
             //robotUtils.circle(50, 50)
->>>>>>> 97f6ffff7927cecf201b6c73a5c8f9c5c8d5093a
         }
     }
 }
