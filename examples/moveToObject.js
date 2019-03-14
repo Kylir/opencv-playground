2794aa07337db5c28e00c320c9b45528d18ca4e9
Kylir <jason.forest@glyphe.org>
2019-03-14 22:36:40 +0000

    keep only logs to debug.

--- examples/moveToObject.js
+++ examples/moveToObject.js
@@ -23,15 +23,15 @@ function demoRobotMove (colorName) {
                 const c = cvUtils.findCentre(big)
                 const deviation = cvUtils.xAxisDeviation(c.cX, 300)
                 console.log(`Big enough! Recenter! Deviation is ${deviation}`)
-                robotUtils.moveToTarget(100, deviation, 0.2)
+                //robotUtils.moveToTarget(100, deviation, 0.2)
             } else {
                 console.log('Nothing big enough. Stop...')
-                robotUtils.stop()
+                //robotUtils.stop()
             }
             
         } else {
             console.log('No objects found. Stop...')
-            robotUtils.stop()
+            //robotUtils.stop()
         }
     }
 }
