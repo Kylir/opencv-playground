25c22dc04a44bb24c13dac2422e7f9c250a0310f
Kylir <jason.forest@glyphe.org>
2019-03-15 00:48:11 +0000

    Log more stuff to debug

--- examples/moveToObject.js
+++ examples/moveToObject.js
@@ -15,11 +15,12 @@ function demoRobotMove (colorName) {
         if (cont && (cont.length > 0)) {
             const big = cvUtils.findBiggestArea(cont)
             console.log(`${cont.length} objectsdetected.`)
+            console.log(`Big: ${big}`)
             console.log(`Biggest at (${big.cX}, ${big.cY}) with area ${big.area}.`)
             
             if (big.area > 500000) {
                 console.log('Too big! Stop...')
-            } else if (big.area > 15) {
+            } else if (big.area > 200) {
                 const c = cvUtils.findCentre(big)
                 const deviation = cvUtils.xAxisDeviation(c.cX, 300)
                 console.log(`Big enough! Recenter! Deviation is ${deviation}`)
