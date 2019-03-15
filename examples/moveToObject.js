94813b303cffb9d2e948325217fb6d275e6fc1f4
Kylir <jason.forest@glyphe.org>
2019-03-15 00:59:57 +0000

    Re-enable motors

--- examples/moveToObject.js
+++ examples/moveToObject.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 const cvUtils = require('../src/opencv-utils')
-//const robotUtils = require('../src/robot-utils')
+const robotUtils = require('../src/robot-utils')
 
 
 function demoRobotMove (colorName) {
@@ -9,14 +9,12 @@ function demoRobotMove (colorName) {
     while (true) {
         const frame = wCap.read()
         const processed = cvUtils.processImage(frame)
-        //const cont = cvUtils.findRedContours(processed)
         const cont = cvUtils.findContoursForColor(processed, colorName)
         
         if (cont && (cont.length > 0)) {
             const big = cvUtils.findBiggestArea(cont)
             console.log(`${cont.length} objectsdetected.`)
-            console.log(`Big: ${JSON.stringify(big)}`)
-            console.log(`Biggest at (${big.cX}, ${big.cY}) with area ${big.area}.`)
+            console.log(`Biggest has area ${big.area}.`)
             
             if (big.area > 500000) {
                 console.log('Too big! Stop...')
@@ -24,15 +22,15 @@ function demoRobotMove (colorName) {
                 const c = cvUtils.findCentre(big)
                 const deviation = cvUtils.xAxisDeviation(c.cX, 300)
                 console.log(`Big enough! Recenter! Deviation is ${deviation}`)
-                //robotUtils.moveToTarget(100, deviation, 0.2)
+                robotUtils.moveToTarget(100, deviation, 0.2)
             } else {
                 console.log('Nothing big enough. Stop...')
-                //robotUtils.stop()
+                robotUtils.stop()
             }
             
         } else {
             console.log('No objects found. Stop...')
-            //robotUtils.stop()
+            robotUtils.stop()
         }
     }
 }
