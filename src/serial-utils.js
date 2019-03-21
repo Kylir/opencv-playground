26ab9281269c881edba5b8a76dd21ca6c4d18ce7
Kylir <jason.forest@glyphe.org>
2019-03-21 22:50:42 +0000

    Add serial

---
+++ src/serial-utils.js
@@ -0,0 +1,12 @@
+const raspi = require('raspi')
+const Serial = require('raspi-serial').Serial
+
+raspi.init(() => {
+  const serial = new Serial({portId: '/dev/ttyS0'})
+  serial.open(() => {
+    serial.on('data', (data) => {
+      process.stdout.write(data)
+    })
+    serial.write('s1')
+  });
+})
