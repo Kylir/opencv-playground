c276ad61d25dd94240410a5d9cf0bf1ce9a1fdd7
Kylir <jason.forest@glyphe.org>
2019-03-30 21:38:07 +0000

    change serial example

--- examples/serial.js
+++ examples/serial.js
@@ -2,7 +2,7 @@ const raspi = require('raspi');
 const Serial = require('raspi-serial').Serial;
  
 raspi.init(() => {
-  const serial = new Serial();
+  const serial = new Serial({portId: '/dev/ttyS0'});
   serial.open(() => {
     serial.on('data', (data) => {
       process.stdout.write(data);
