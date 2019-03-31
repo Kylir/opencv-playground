60c4c807a2e462774419b23b14bb6c0ef3a059b3
Kylir <jason.forest@glyphe.org>
2019-03-31 00:34:43 +0000

    change deviation logic

--- src/robot-utils.js
+++ src/robot-utils.js
@@ -4,8 +4,22 @@ const piwars = require('./piwars-utils')
 
 function moveToTarget (baseSpeed, deviation, k, serial) {
     
-    const right = Math.floor(baseSpeed + (k * deviation))
-    const left = Math.floor(baseSpeed - (k * deviation))
+    //const right = Math.floor(baseSpeed + (k * deviation))
+    //const left = Math.floor(baseSpeed - (k * deviation))
+
+    let right
+    let left
+
+    if (deviation > 10) {
+        right = baseSpeed
+        left = baseSpeed - 10
+    } else if (deviation < -10) {
+        right = baseSpeed -10
+        left = baseSpeed
+    } else {
+        right = baseSpeed
+        left = baseSpeed
+    }
 
     console.log(`Parameters: baseSpeed=${baseSpeed}, deviation=${deviation}, k=${k}`)
     console.log(`piwars.drive(${right}, ${left})`)
@@ -19,12 +33,12 @@ function stop (serial) {
 
 function circle (serial) {
     console.log('Circling...')
-    piwars.drive(30, 0, serial)
+    piwars.drive(50, 0, serial)
 }
 
 function circleOpposite (serial) {
     console.log('Circling in the opposite direction...')
-    piwars.drive(0, 30, serial)
+    piwars.drive(0, 50, serial)
 }
 
 function back(serial) {
